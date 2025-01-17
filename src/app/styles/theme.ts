613e32f72575871dd4c5af89366baae6e293ed00
angel C <cussi.angel.benjamin@gmail.com>
2025-01-16 22:44:06 -0400

    initial global theme

---
+++ src/app/styles/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+    colors: {
+      primary: "#16468A",
+      secondary: "#00B383",
+      background: "#F5F5F5",
+      text: "#333333",
+      white: "#FFFFFF",
+      error: "#ff4d4f"
+    },
+    fonts: {
+      primary: "'Open Sans', sans-serif"
+    },
+    breakpoints: {
+      mobile: "480px",
+      tablet: "768px",
+      desktop: "1024px"
+    }
+  };
+  
