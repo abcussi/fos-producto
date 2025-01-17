69625c99ca08c02ae1761cdc73343606eae2bd47
angel C <cussi.angel.benjamin@gmail.com>
2025-01-17 13:34:16 -0400

    remove cookie

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,9 +1,8 @@
+import cookies from 'js-cookie';
 import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
 
 export async function POST() {
-  const cookieStore = cookies();
-  (await cookieStore).delete('token');
+  cookies.remove('token');
 
   return NextResponse.json({ success: true });
 }
